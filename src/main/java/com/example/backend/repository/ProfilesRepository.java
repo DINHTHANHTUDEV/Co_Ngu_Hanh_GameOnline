1c2a829f3c57396ff48802f83e6123914338eeae
TÚ IT <dinhthanhtu26012005@gmail.com>
2025-05-14 09:58:45 +0700

    Update Application MySQL

--- src/main/java/com/example/backend/repository/ProfilesRepository.java
+++ src/main/java/com/example/backend/repository/ProfilesRepository.java
@@ -11,22 +11,22 @@ import java.util.List;
 @Repository
 public interface ProfilesRepository extends JpaRepository<Profiles,Integer> {
     @Query("""
-select new com.example.backend.DTO.ProfilesResponse (
-pf.user_id,
-pf.total_matches,
-pf.wins,
-pf.winrate,
-pf.current_stars,
-pf.elo_points,
-pf.image_user,
-pf.level_id.level_id,
-pf.level_id.level_name,
-pf.level_id.required_matches,
-pf.level_id.required_winrate,
-pf.level_id.reward_stars,
-pf.level_id.stars_bet
-
-) from Profiles pf
-""")
+            select new com.example.backend.DTO.ProfilesResponse (
+            pf.user_id,
+            pf.total_matches,
+            pf.wins,
+            pf.winrate,
+            pf.current_stars,
+            pf.elo_points,
+            pf.image_user,
+            pf.level_id.level_id,
+            pf.level_id.level_name,
+            pf.level_id.required_matches,
+            pf.level_id.required_winrate,
+            pf.level_id.reward_stars,
+            pf.level_id.stars_bet
+            
+            ) from Profiles pf
+            """)
     public List<ProfilesResponse> getAllProfiles();
 }
